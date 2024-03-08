cf3a739d0c3a9365f1f64fdc1bf718d77bc00cee
Nik Sirotinin <mega42@gmail.com>
2024-03-08 23:51:15 +0000

    Try to make the API behave

--- api/main.py
+++ api/main.py
@@ -1,11 +1,14 @@
 import asyncio
 from random import randrange
 from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(CORSMiddleware, allow_methods=["OPTIONS", "PUT"])
 
-@app.post("/isthisacat/")
+
+@app.put("/isthisacat/")
 async def isthisacat(file: UploadFile):
     if file.content_type != "image/jpeg":
         raise HTTPException(status_code=400, detail="Wrong file format")
