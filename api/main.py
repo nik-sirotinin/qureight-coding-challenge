d19f8dddc7a9819a6025a8d3e2d6d6ee8c046c10
Nik Sirotinin <mega42@gmail.com>
2024-03-12 12:37:22 +0000

    Tweak sleep and messages

--- api/main.py
+++ api/main.py
@@ -17,9 +17,9 @@ async def isthisacat(file: UploadFile):
     if file.content_type != "image/jpeg":
         raise HTTPException(status_code=400, detail="Wrong file format")
 
-    await asyncio.sleep(5)
+    await asyncio.sleep(60)
     confidence = randrange(100)
     success = confidence > 50
-    info = "Probably a cat" if success else "Doesn't look like a cat"
+    info = "Probably a cat." if success else "Doesn't look like a cat."
 
     return dict(success=success, confidence=confidence, info=info)
